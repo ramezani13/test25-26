6883ed93df83c703bcb9560d5f835370a34c9ba7
ramezani13 <147407440+ramezani13@users.noreply.github.com>
2025-10-22 13:26:53 +0200

    Delete is_odd.py

--- is_odd.py
+++
@@ -1,7 +0,0 @@
-def is_odd(k):
-    """
-    Return True if k is odd, otherwise return False.
-    """
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
