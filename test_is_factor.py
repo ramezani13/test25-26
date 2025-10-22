db961b0661cc65c0ddbd629d301084502902daf4
ramezani13 <147407440+ramezani13@users.noreply.github.com>
2025-10-22 13:28:27 +0200

    test_is_factor.py

---
+++ test_is_factor.py
@@ -0,0 +1,25 @@
+from is_factor import *
+
+def test_factor1():
+    assert is_factor(1, 1) is True
+
+def test_factor2():
+    assert is_factor(2, 10) is True
+
+def test_factor3():
+    assert is_factor(-5, 25) is True
+
+def test_factor4():
+    assert is_factor(5, 0) is False
+
+def test_factor5():
+    assert is_factor(0, 0) is False
+
+def test_factor6():
+    assert is_factor(2, 11) is False
+
+def test_factor7():
+    assert is_factor(10, 2) is False
+
+def test_factor8():
+    assert is_factor(0, 5) is False
