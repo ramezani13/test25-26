13002fbb31245329b9f93e497773063d3bd3d1a5
ramezani13 <147407440+ramezani13@users.noreply.github.com>
2025-10-22 13:25:53 +0200

    is_factor.py

--- count_odd_digits.py
+++
@@ -1,4 +0,0 @@
-def count_odd_digits(n):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
---
+++ is_factor.py
@@ -0,0 +1,7 @@
+def is_factor(n, m):
+    if n == 0:
+        return False
+    elif m == 0:
+        return False
+    else:       
+        return not (m % n)
